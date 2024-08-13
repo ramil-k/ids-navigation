fa0c371a34fbfe819fd37cfaf02412b5e334a075
Ramil Karimov <ramil1017@gmail.com>
2024-08-13 18:38:33 +0500

    Updates

--- _app/immutable/chunks/paths.DrctVnlc.js
+++
@@ -1,1 +0,0 @@
-var s;const e=((s=globalThis.__sveltekit_4erh9b)==null?void 0:s.base)??"/ids-navigation";var a;const t=((a=globalThis.__sveltekit_4erh9b)==null?void 0:a.assets)??e;export{t as a,e as b};
---
+++ _app/immutable/entry/start.i8GMcsjg.js
@@ -0,0 +1,1 @@
+import{a as t}from"../chunks/entry.1P0sb54P.js";export{t as start};
