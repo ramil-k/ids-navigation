4acc8ca6022671e15e0fd6a014374a5aa9b160ce
Ramil Karimov <ramil1017@gmail.com>
2024-08-15 19:52:37 +0500

    Updates

---
+++ _app/immutable/chunks/paths.N-XSkVvE.js
@@ -0,0 +1,1 @@
+var s;const t=((s=globalThis.__sveltekit_sv9xiy)==null?void 0:s.base)??"/ids-navigation";var a;const e=((a=globalThis.__sveltekit_sv9xiy)==null?void 0:a.assets)??t;export{e as a,t as b};
